95efcf38c69a5559bf36572298c58568469f7d02
Francowtzl <francowtzl@gmail.com>
2022-12-10 11:48:24 -0300

    Principio MVTWetzelFranco

---
+++ AppWetzel/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class FliaWetzel(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    edad=models.IntegerField()
